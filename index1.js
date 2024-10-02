90bca9209dcc35838580010c5e6d2dc2312ec9fc
Phuc2003-vietnam <phuc.nguyen180728@hcmut.edu.vn>
2024-10-02 09:42:08 +0700

    bb

--- index1.js
+++ index1.js
@@ -1,6 +1,7 @@
-console.log("starting main")
-console.log("main 2")
-console.log("main 3")
-console.log("starting feature")
-console.log("starting feature 2")
-console.log("starting feature 3")
+console.log("starting main");
+console.log("main 2");
+console.log("main 3");
+console.log("starting feature");
+console.log("starting feature 2");
+console.log("starting feature 3");
+console.log("bb");
